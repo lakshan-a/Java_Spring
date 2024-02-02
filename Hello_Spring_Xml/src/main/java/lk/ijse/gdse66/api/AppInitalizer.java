bf472c6d6b3359017e2207c31cabf622fc5d5690
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-02 15:10:09 +0530

    updated

--- Hello_Spring_Xml/src/main/java/lk/ijse/gdse66/api/AppInitalizer.java
+++ Hello_Spring_Xml/src/main/java/lk/ijse/gdse66/api/AppInitalizer.java
@@ -1,10 +1,13 @@
 package lk.ijse.gdse66.api;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class AppInitalizer {
     public static void main(String[] args) {
 
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext();
+
+        AnnotationConfigApplicationContext context1 = new AnnotationConfigApplicationContext("context.xml");
     }
 }
