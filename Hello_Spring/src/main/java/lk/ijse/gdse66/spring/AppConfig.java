d8804bfaaabd7f70e39de389c7e3395f142891fa
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-02 12:12:50 +0530

    updated

--- Hello_Spring/src/main/java/lk/ijse/gdse66/spring/AppConfig.java
+++ Hello_Spring/src/main/java/lk/ijse/gdse66/spring/AppConfig.java
@@ -1,8 +1,10 @@
 package lk.ijse.gdse66.spring;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan
 public class AppConfig {
     public AppConfig(){
         System.out.println("appConfig instance is created");
--- Hello_Spring/src/main/java/lk/ijse/gdse66/spring/bean/SpringBean.java
+++ Hello_Spring/src/main/java/lk/ijse/gdse66/spring/bean/SpringBean.java
@@ -1,5 +1,8 @@
 package lk.ijse.gdse66.spring.bean;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class SpringBean {
     public SpringBean(){
         System.out.println("SpringBean instance is created");
