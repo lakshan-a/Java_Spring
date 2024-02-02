638afdb311b58ba1f556ff15a3d41f92820aa12c
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-02 11:40:16 +0530

    updated

--- Hello_Spring/src/main/java/lk/ijse/gdse66/spring/AppConfig.java
+++ Hello_Spring/src/main/java/lk/ijse/gdse66/spring/AppConfig.java
@@ -1,4 +1,10 @@
 package lk.ijse.gdse66.spring;
 
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class AppConfig {
+    public AppConfig(){
+        System.out.println("appConfig instance");
+    }
 }
