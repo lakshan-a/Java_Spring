267ae47755017e72fa98c154248ffb7b34adcd40
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-02 15:06:50 +0530

    updated

--- Hello_Spring_Xml/src/main/java/lk/ijse/gdse66/api/AppInitalizer.java
+++ Hello_Spring_Xml/src/main/java/lk/ijse/gdse66/api/AppInitalizer.java
@@ -1,4 +1,10 @@
 package lk.ijse.gdse66.api;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class AppInitalizer {
+    public static void main(String[] args) {
+
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext();
+    }
 }
--- Hello_Spring_Xml/src/main/java/lk/ijse/gdse66/api/bean/SpringBean.java
+++ Hello_Spring_Xml/src/main/java/lk/ijse/gdse66/api/bean/SpringBean.java
@@ -1,4 +1,8 @@
 package lk.ijse.gdse66.api.bean;
 
 public class SpringBean {
+
+    public SpringBean() {
+        System.out.println("SpringBean instance is created");
+    }
 }
