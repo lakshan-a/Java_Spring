d66d5f1f0e57e8b6f253c38f58fee7b3fb3408c6
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-02 11:43:53 +0530

    updated

--- Hello_Spring/src/main/java/lk/ijse/gdse66/spring/AppInitializer.java
+++ Hello_Spring/src/main/java/lk/ijse/gdse66/spring/AppInitializer.java
@@ -2,15 +2,14 @@ package lk.ijse.gdse66.spring;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import java.lang.reflect.AnnotatedArrayType;
 
-public class Applnitializer {
+public class AppInitializer {
 
     public static void main(String[] args) {
 
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
 
         ctx.register(AppConfig.class);
-        ctx.register();
+        ctx.refresh();
     }
 }
