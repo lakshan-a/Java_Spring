adf377b3be77a402ce3fa89494a6d495f5eeaa7b
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-09 16:43:04 +0530

    updated

--- 04-Dependency_Injection/src/main/java/lk/ijse/api/spring/AppInitializer.java
+++ 04-Dependency_Injection/src/main/java/lk/ijse/api/spring/AppInitializer.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class AppInitializer {
     public static void main(String[] args) {
+
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
         ctx.register(AppConfig.class);
         ctx.refresh();
--- 04-Dependency_Injection/src/main/java/lk/ijse/api/spring/bean/Boy.java
+++ 04-Dependency_Injection/src/main/java/lk/ijse/api/spring/bean/Boy.java
@@ -1,7 +1,13 @@
 package lk.ijse.api.spring.bean;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 
 /**
  * @version: v0.0.1
@@ -10,7 +16,7 @@ import org.springframework.stereotype.Component;
  */
 
 @Component("Lahiru")
-public class Boy {
+public class Boy implements BeanNameAware, BeanFactoryAware, ApplicationContextAware, InitializingBean, DisposableBean {
 
     @Autowired
     private GoodGirl gf;
@@ -18,5 +24,35 @@ public class Boy {
     public Boy() {
         System.out.println("Boy - Constructor");
         System.out.println("Have I got a Girl Friend? " + gf);
+    }
+
+    @PostConstruct
+    public void initialize(){
+        System.out.println("Have I got a Girl Friend? " + gf);
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+
+    }
+
+    @Override
+    public void setBeanName(String s) {
+
+    }
+
+    @Override
+    public void destroy() throws Exception {
+
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+
     }
 }
