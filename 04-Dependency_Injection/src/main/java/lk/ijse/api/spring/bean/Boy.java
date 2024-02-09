77941578cde5d98eb41dff9bc207f30ee1fbe616
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-09 16:28:01 +0530

    updated

---
+++ 04-Dependency_Injection/src/main/java/lk/ijse/api/spring/bean/Boy.java
@@ -0,0 +1,13 @@
+package lk.ijse.api.spring.bean;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @version: v0.0.1
+ * @author: LakshanR
+ * @date: 2/9/2024
+ */
+
+@Component("Lahiru")
+public class Boy {
+}
