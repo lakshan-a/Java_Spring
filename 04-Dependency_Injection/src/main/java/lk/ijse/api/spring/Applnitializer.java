d0a94dd48534f2a8e549c79cfe5c2daec3138dbb
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-09 16:13:07 +0530

    updated

--- 04-Dependency_Injection/src/main/java/lk/ijse/api/spring/Applnitializer.java
+++ 04-Dependency_Injection/src/main/java/lk/ijse/api/spring/Applnitializer.java
@@ -1,9 +1,18 @@
 package lk.ijse.api.spring;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
  * @version: v0.0.1
  * @author: LakshanR
  * @date: 2/9/2024
  */
 public class Applnitializer {
+    public static void main(String[] args) {
+
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+        ctx.register(AppConfig.class);
+        ctx.refresh();
+
+    }
 }
