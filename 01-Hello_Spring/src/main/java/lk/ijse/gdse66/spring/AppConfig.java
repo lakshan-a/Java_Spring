0c88279787e5a707401357b50217da4350a2b283
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-09 10:52:02 +0530

    updated

--- 01-Hello_Spring/src/main/java/lk/ijse/gdse66/spring/AppConfig.java
+++ 01-Hello_Spring/src/main/java/lk/ijse/gdse66/spring/AppConfig.java
@@ -1,11 +1,12 @@
 package lk.ijse.gdse66.spring;
 
+import com.edu.ijse.SpringBean4;
 import lk.ijse.gdse66.spring.bean.SpringBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 //@ComponentScan(basePackages = "lk.ijse.gdse66.spring.bean")
-@ComponentScan(basePackageClasses = SpringBean.class)
+@ComponentScan(basePackageClasses = {SpringBean3.class, SpringBean4.class})
 @Configuration
 
 public class AppConfig {
