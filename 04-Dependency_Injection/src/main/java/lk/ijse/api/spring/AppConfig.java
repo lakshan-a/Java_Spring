43e46400a346362603ada3a73c83008a137d90ff
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-09 16:26:34 +0530

    updated

---
+++ 04-Dependency_Injection/src/main/java/lk/ijse/api/spring/AppConfig.java
@@ -0,0 +1,15 @@
+package lk.ijse.api.spring;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @version: v0.0.1
+ * @author: LakshanR
+ * @date: 2/9/2024
+ */
+
+@Configuration
+@ComponentScan
+public class AppConfig {
+}
--- 04-Dependency_Injection/src/main/java/lk/ijse/api/spring/AppInitializer.java
+++ 04-Dependency_Injection/src/main/java/lk/ijse/api/spring/AppInitializer.java
@@ -1,17 +1,18 @@
 package lk.ijse.api.spring;
 
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
  * @version: v0.0.1
  * @author: LakshanR
  * @date: 2/9/2024
  */
+
 public class AppInitializer {
     public static void main(String[] args) {
-
-        Girl lakshan = new lakshan();
-        lakshan.Girl();
-
-
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+        ctx.register(AppConfig.class);
+        ctx.refresh();
     }
 }
