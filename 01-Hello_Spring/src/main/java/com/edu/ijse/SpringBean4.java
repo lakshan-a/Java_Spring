d03f21c8fc7ccc655579c4fb9bff2521a0167364
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-09 10:47:25 +0530

    updated

--- 01-Hello_Spring/src/main/java/com/edu/ijse/SpringBean4.java
+++ 01-Hello_Spring/src/main/java/com/edu/ijse/SpringBean4.java
@@ -1,4 +1,10 @@
 package com.edu.ijse;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class SpringBean4 {
+    public SpringBean4() {
+        System.out.println("com.edu.ijse.SpringBean4");
+    }
 }
