1615070edb2a591be5ad75265491a7a727c02d5a
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-16 12:19:21 +0530

    updated

--- 04-Dependency_Injection/src/main/java/lk/ijse/api/spring/bean/Sachini.java
+++ 04-Dependency_Injection/src/main/java/lk/ijse/api/spring/bean/Sachini.java
@@ -4,6 +4,7 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.*;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 /**
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Component;
  */
 
 @Component
+@Primary
 public class Sachini implements GoodGirl, BeanNameAware, BeanFactoryAware, ApplicationContextAware, InitializingBean, DisposableBean {
 
 
