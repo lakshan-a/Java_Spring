b4c0ebff112a3f5aa5de6ddf549bec7b7b363ff3
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-16 12:26:56 +0530

    updated

--- 04-Dependency_Injection/src/main/java/lk/ijse/api/spring/bean/Boy.java
+++ 04-Dependency_Injection/src/main/java/lk/ijse/api/spring/bean/Boy.java
@@ -4,6 +4,7 @@ import lk.ijse.api.spring.util.Injector;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
@@ -43,7 +44,7 @@ public class Boy implements /*Injector,*/ BeanNameAware, BeanFactoryAware, Appli
     }*/
 
     @Autowired
-    public Boy(GoodGirl gf){
+    public Boy(@Qualifier("Long Hair") GoodGirl gf){
         this.gf=gf;
         System.out.println("Boy - Constructor");
         System.out.println("Have I got a Girl Friend? " + gf);
