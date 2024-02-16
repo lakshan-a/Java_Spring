20bd2605e51e3d36aba4f10801d6ba732380d4d1
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-16 11:00:27 +0530

    updated

--- 04-Dependency_Injection/src/main/java/lk/ijse/api/spring/bean/Sachini.java
+++ 04-Dependency_Injection/src/main/java/lk/ijse/api/spring/bean/Sachini.java
@@ -29,26 +29,26 @@ public class Sachini implements GoodGirl, BeanNameAware, BeanFactoryAware, Appli
 
     @Override
     public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
-
+        System.out.println("Girl- setBeanFactory..............");
     }
 
     @Override
     public void setBeanName(String s) {
-
+        System.out.println("Girl- setBeanName...............");
     }
 
     @Override
     public void destroy() throws Exception {
-
+        System.out.println("Girl- destroy.............");
     }
 
     @Override
     public void afterPropertiesSet() throws Exception {
-
+        System.out.println("Girl- afterPropertiesSet.................");
     }
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-
+        System.out.println("Girl-setApplicationContext..................");
     }
 }
