c4cb4bf034852046cf1561043c1e20d24046e9dc
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-16 12:04:41 +0530

    updated

--- 04-Dependency_Injection/src/main/java/lk/ijse/api/spring/bean/Tharushi.java
+++ 04-Dependency_Injection/src/main/java/lk/ijse/api/spring/bean/Tharushi.java
@@ -5,5 +5,9 @@ package lk.ijse.api.spring.bean;
  * @author: LakshanR
  * @date: 2/16/2024
  */
-public class Tharushi {
+public class Tharushi implements GoodGirl{
+    @Override
+    public void love() {
+        System.out.println("Tharushi loves him so much");
+    }
 }
