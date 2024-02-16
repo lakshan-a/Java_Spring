aee14b78211886b60d8adf14d6104b2bc4e6d1ce
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-16 15:30:59 +0530

    updated

--- 04-Dependency_Injection/src/main/java/lk/ijse/api/spring/bean/Tharushi.java
+++ 04-Dependency_Injection/src/main/java/lk/ijse/api/spring/bean/Tharushi.java
@@ -15,6 +15,10 @@ import org.springframework.stereotype.Component;
 @Qualifier("Long Hair")
 public class Tharushi implements GoodGirl{
 
+    public Tharushi() {
+        System.out.println("Tharushi - Constructor");
+    }
+
     @Override
     public void love() {
         System.out.println("Tharushi loves him so much");
