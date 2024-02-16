b84084f18c2b6840f5a432cac5afc42d98c77320
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-16 12:48:01 +0530

    updated

--- 04-Dependency_Injection/src/main/java/lk/ijse/api/spring/util/WhiteFace.java
+++ 04-Dependency_Injection/src/main/java/lk/ijse/api/spring/util/WhiteFace.java
@@ -1,11 +1,19 @@
 package lk.ijse.api.spring.util;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import java.lang.annotation.*;
+
 /**
  * @version: v0.0.1
  * @author: LakshanR
  * @date: 2/16/2024
  */
 
-
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.TYPE, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Documented
+@Qualifier
 public @interface WhiteFace {
 }
