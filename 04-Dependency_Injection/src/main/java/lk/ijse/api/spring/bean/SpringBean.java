26a0182adefb6773b91b48fec579a875bb34d999
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-16 15:21:13 +0530

    updated

---
+++ 04-Dependency_Injection/src/main/java/lk/ijse/api/spring/bean/SpringBean.java
@@ -0,0 +1,16 @@
+package lk.ijse.api.spring.bean;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @version: v0.0.1
+ * @author: LakshanR
+ * @date: 2/16/2024
+ */
+
+@Component
+public class SpringBean {
+
+    public SpringBean() {
+    }
+}
