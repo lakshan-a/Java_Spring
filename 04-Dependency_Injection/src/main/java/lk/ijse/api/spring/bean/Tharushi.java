fe4e17e2c65f1250de407458934ee1253c4b747b
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-16 12:06:50 +0530

    updated

--- 04-Dependency_Injection/src/main/java/lk/ijse/api/spring/bean/Tharushi.java
+++ 04-Dependency_Injection/src/main/java/lk/ijse/api/spring/bean/Tharushi.java
@@ -1,11 +1,16 @@
 package lk.ijse.api.spring.bean;
 
+import org.springframework.stereotype.Component;
+
 /**
  * @version: v0.0.1
  * @author: LakshanR
  * @date: 2/16/2024
  */
+
+@Component
 public class Tharushi implements GoodGirl{
+
     @Override
     public void love() {
         System.out.println("Tharushi loves him so much");
