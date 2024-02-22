52c8bc209c59a99f36299350c47484b6ba19249a
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-22 11:52:54 +0530

    updated

---
+++ 06-Spring-Configaration/src/main/java/lk/ijse/api/SpringConfiguration/Config1.java
@@ -0,0 +1,15 @@
+package lk.ijse.api.SpringConfiguration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @version: v0.0.1
+ * @author: LakshanR
+ * @date: 2/22/2024
+ */
+
+@Configuration
+@ComponentScan
+public class Config1 {
+}
