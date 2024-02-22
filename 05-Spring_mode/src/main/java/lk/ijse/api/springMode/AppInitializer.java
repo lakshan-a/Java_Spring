98c19c2fe2ca6f61f50baded099cb18417ae7d7c
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-22 09:26:06 +0530

    updated

--- 05-Spring_mode/src/main/java/lk/ijse/api/springMode/AppInitializer.java
+++ 05-Spring_mode/src/main/java/lk/ijse/api/springMode/AppInitializer.java
@@ -1,5 +1,7 @@
 package lk.ijse.api.springMode;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
  * @version: v0.0.1
  * @author: LakshanR
@@ -7,6 +9,8 @@ package lk.ijse.api.springMode;
  */
 public class AppInitializer {
     public static void main(String[] args) {
-        System.out.println("Spring Mode");
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+        ctx.register(AppConfig.class);
+        ctx.refresh();
     }
 }
