9ca4f47647e0786773c6f3166f86714902e05264
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-22 14:00:13 +0530

    updated

--- 07-RunTime-Value-Injection/src/main/java/lk/ijse/api/RunTimeInjection/bean/SpringBean1.java
+++ 07-RunTime-Value-Injection/src/main/java/lk/ijse/api/RunTimeInjection/bean/SpringBean1.java
@@ -20,8 +20,16 @@ public class SpringBean1 {
     @Value("Amal")
     private String name;
 
-    public SpringBean1 (@Value("IJSE") String myString){
+    public SpringBean1 (@Value("IJSE") String myString,
+                        @Value("6") int myInt,
+                        @Value("6.2") double myDouble,
+                        @Value("true") boolean myBoolean,
+                        @Value("1,2,3,4,5") int[] myIntArray){
         System.out.println("SpringBean1() - Constructor : " + myString);
+        System.out.println("SpringBean1() - Constructor : " + myInt);
+        System.out.println("SpringBean1() - Constructor : " + myDouble);
+        System.out.println("SpringBean1() - Constructor : " + myBoolean);
+        System.out.println("SpringBean1() - Constructor : " + myIntArray);
         System.out.println("name :" + name);
     }
 
@@ -29,7 +37,5 @@ public class SpringBean1 {
     public void initialize(){
         System.out.println("name : " +name);
     }
-
-
 
 }
