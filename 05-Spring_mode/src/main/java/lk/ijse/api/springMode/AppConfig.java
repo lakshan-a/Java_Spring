97c200ed3888b5d65e62d836680b765d346ef85b
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-22 09:30:53 +0530

    updated

--- 05-Spring_mode/src/main/java/lk/ijse/api/springMode/AppConfig.java
+++ 05-Spring_mode/src/main/java/lk/ijse/api/springMode/AppConfig.java
@@ -1,9 +1,16 @@
 package lk.ijse.api.springMode;
 
+import org.springframework.stereotype.Component;
+
 /**
  * @version: v0.0.1
  * @author: LakshanR
  * @date: 2/22/2024
  */
+
+@Component
 public class AppConfig {
+    public AppConfig() {
+        System.out.println("Spring Bean One");
+    }
 }
