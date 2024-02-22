2767da95c449a5ea021f6d9c7a3e27895ecc7e8b
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-22 12:06:43 +0530

    updated

--- 06-Spring-Configaration/src/main/java/lk/ijse/api/SpringConfiguration/bean/AnnotationConfigSpring.java
+++ 06-Spring-Configaration/src/main/java/lk/ijse/api/SpringConfiguration/bean/AnnotationConfigSpring.java
@@ -1,11 +1,15 @@
 package lk.ijse.api.SpringConfiguration.bean;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
 /**
  * @version: v0.0.1
  * @author: LakshanR
  * @date: 2/22/2024
  */
 
+@Configuration
 public class AnnotationConfigSpring {
 
     public AnnotationConfigSpring() {
