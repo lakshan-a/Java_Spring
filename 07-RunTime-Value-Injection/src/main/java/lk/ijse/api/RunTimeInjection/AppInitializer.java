2c6923015f311c1f6aa59f5f1234eecf9902767b
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-22 15:30:58 +0530

    updated

--- 07-RunTime-Value-Injection/src/main/java/lk/ijse/api/RunTimeInjection/AppInitializer.java
+++ 07-RunTime-Value-Injection/src/main/java/lk/ijse/api/RunTimeInjection/AppInitializer.java
@@ -21,7 +21,6 @@ public class AppInitializer {
 
         System.out.println("========================================");
         Map<String,String> map = System.getenv();
-//        System.out.println(map);
         for (String keys : map.keySet()){
             System.out.println(keys + "=" + map.get(keys));
         }
