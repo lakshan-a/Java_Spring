1807d4ea6e003dee75b3aa2c1e64b62e9ee1081e
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-22 12:42:14 +0530

    updated

--- 07-RunTime-Value-Injection/src/main/java/lk/ijse/api/RunTimeInjection/bean/SpringBean1.java
+++ 07-RunTime-Value-Injection/src/main/java/lk/ijse/api/RunTimeInjection/bean/SpringBean1.java
@@ -1,9 +1,17 @@
 package lk.ijse.api.RunTimeInjection.bean;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
 /**
  * @version: v0.0.1
  * @author: LakshanR
  * @date: 2/22/2024
  */
+
+@Configuration
 public class SpringBean1 {
+    public SpringBean1() {
+        System.out.println("SpringBean1() - Constructor");
+    }
 }
