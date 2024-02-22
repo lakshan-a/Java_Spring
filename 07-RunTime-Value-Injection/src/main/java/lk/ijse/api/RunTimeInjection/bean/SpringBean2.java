f305029a1176a3f1a36d270691121a3be1a00cd6
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-22 15:49:13 +0530

    updated

--- 07-RunTime-Value-Injection/src/main/java/lk/ijse/api/RunTimeInjection/bean/SpringBean2.java
+++ 07-RunTime-Value-Injection/src/main/java/lk/ijse/api/RunTimeInjection/bean/SpringBean2.java
@@ -16,13 +16,19 @@ public class SpringBean2 {
     public SpringBean2(@Value("${USERNAME}") String username,
                        @Value("${USERDOMAIN}") String user,
                        @Value("${POWERSHELL_DISTRIBUTION_CHANNEL}") String dist,
-                       @Value("${java.version}") String java) {
+                       @Value("${java.version}") String java,
+                       @Value("${app.env1}") String env1,
+                       @Value("${app.env2}") String env2,
+                       @Value("${app.env3}") String env3) {
 
         System.out.println("SpringBean2() - Constructor");
         System.out.println("username : " + username);
         System.out.println("USERDOMAIN : " + user);
         System.out.println("Powershell : " + dist);
         System.out.println("version : " + java);
+        System.out.println("app.env1 : " + env1);
+        System.out.println("app.env2 : " + env2);
+        System.out.println("app.env3 : " + env3);
 
     }
 }
