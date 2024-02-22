8c2dc4aa5c3b429a724b1781ce6db16c1aef5aca
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-22 11:54:32 +0530

    updated

--- 06-Spring-Configaration/src/main/java/lk/ijse/api/SpringConfiguration/AppInitializer.java
+++ 06-Spring-Configaration/src/main/java/lk/ijse/api/SpringConfiguration/AppInitializer.java
@@ -11,6 +11,8 @@ public class AppInitializer {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
         ctx.register(AppConfig.class);
+        ctx.register(Config1.class);
+        ctx.register(Config2.class);
         ctx.refresh();
         ctx.registerShutdownHook();
 
