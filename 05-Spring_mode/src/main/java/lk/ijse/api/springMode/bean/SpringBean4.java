4493bc886ea9a30356d07efef5e82de399c3e3e7
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-22 10:04:06 +0530

    updated

--- 05-Spring_mode/src/main/java/lk/ijse/api/springMode/bean/SpringBean4.java
+++ 05-Spring_mode/src/main/java/lk/ijse/api/springMode/bean/SpringBean4.java
@@ -14,34 +14,34 @@ import org.springframework.context.ApplicationContextAware;
 
 public class SpringBean4 implements BeanNameAware, BeanFactoryAware, ApplicationContextAware, InitializingBean, DisposableBean {
     public SpringBean4() {
-        System.out.println("SpringBean4() - Constructor ");
+        System.out.println("SpringBean4() - Constructor : " +this);
     }
 
     @Override
     public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
-        System.out.println("SpringBean4 - setBeanFactory : "+this );
+        System.out.println("SpringBean4 - setBeanFactory : " + this );
     }
 
     @Override
     public void setBeanName(String s) {
-        System.out.println("SpringBean4 - setBeanName : " +this );
+        System.out.println("SpringBean4 - setBeanName : " + this );
 
     }
 
     @Override
     public void destroy() throws Exception {
-        System.out.println("SpringBean4 - destroy : "+this);
+        System.out.println("SpringBean4 - destroy : " + this);
 
     }
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        System.out.println("SpringBean4 - afterPropertiesSet : " +this);
+        System.out.println("SpringBean4 - afterPropertiesSet : " + this);
 
     }
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        System.out.println("SpringBean4 - setApplicationContext : " +this);
+        System.out.println("SpringBean4 - setApplicationContext : " + this);
     }
 }
