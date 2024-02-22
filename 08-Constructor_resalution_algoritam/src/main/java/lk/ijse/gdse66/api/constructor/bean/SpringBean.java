dcb3b6e68f0064b922210d0c06db098c203ce84c
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-22 16:46:34 +0530

    updated

--- 08-Constructor_resalution_algoritam/src/main/java/lk/ijse/gdse66/api/constructor/bean/SpringBean.java
+++ 08-Constructor_resalution_algoritam/src/main/java/lk/ijse/gdse66/api/constructor/bean/SpringBean.java
@@ -10,7 +10,5 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SpringBean {
-    public SpringBean() {
-        System.out.println("SpringBean() - Constructor");
-    }
+
 }
