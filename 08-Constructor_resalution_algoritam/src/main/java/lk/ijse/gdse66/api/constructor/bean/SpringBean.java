a0bd423fc987ca8109d83d0fea0ae469e6f88d31
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-22 16:42:20 +0530

    updated

---
+++ 08-Constructor_resalution_algoritam/src/main/java/lk/ijse/gdse66/api/constructor/bean/SpringBean.java
@@ -0,0 +1,12 @@
+package lk.ijse.gdse66.api.constructor.bean;
+
+/**
+ * @version: v0.0.1
+ * @author: LakshanR
+ * @date: 2/22/2024
+ */
+public class SpringBean {
+    public SpringBean() {
+        System.out.println();
+    }
+}
