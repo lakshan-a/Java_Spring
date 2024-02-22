38f863b2bc572a37970cc9a173089f45da70f2fe
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-22 09:31:37 +0530

    updated

--- 05-Spring_mode/src/main/java/lk/ijse/api/springMode/bean/SpringBeanOne.java
+++ 05-Spring_mode/src/main/java/lk/ijse/api/springMode/bean/SpringBeanOne.java
@@ -1,9 +1,16 @@
 package lk.ijse.api.springMode.bean;
 
+import org.springframework.stereotype.Component;
+
 /**
  * @version: v0.0.1
  * @author: LakshanR
  * @date: 2/22/2024
  */
+
+@Component
 public class SpringBeanOne {
+    public SpringBeanOne() {
+        System.out.println("Spring Bean One ");
+    }
 }
