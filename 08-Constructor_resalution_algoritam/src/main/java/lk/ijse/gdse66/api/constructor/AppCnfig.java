2e68186dd3e2dd94644bececce40f8859199ddd0
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-22 16:41:25 +0530

    updated

--- 08-Constructor_resalution_algoritam/src/main/java/lk/ijse/gdse66/api/constructor/AppCnfig.java
+++ 08-Constructor_resalution_algoritam/src/main/java/lk/ijse/gdse66/api/constructor/AppCnfig.java
@@ -5,8 +5,5 @@ package lk.ijse.gdse66.api.constructor;
  * @author: LakshanR
  * @date: 2/22/2024
  */
-public class AppInitializer {
-    public static void main(String[] args) {
-
-    }
+public class AppCnfig {
 }
