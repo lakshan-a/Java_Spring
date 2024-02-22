e4f21c2f0f0f7d21770774cb57e1220211493bf9
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-22 16:15:32 +0530

    updated

--- 07-RunTime-Value-Injection/src/main/java/lk/ijse/api/RunTimeInjection/bean/SpringBean3.java
+++ 07-RunTime-Value-Injection/src/main/java/lk/ijse/api/RunTimeInjection/bean/SpringBean3.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+
 /**
  * @version: v0.0.1
  * @author: LakshanR
@@ -20,7 +22,14 @@ public class SpringBean3 {
         System.out.println("SpringBean3() - Constructor");
     }
 
+    @PostConstruct
     public void initialize(){
         System.out.println(env.getProperty("USERNAME"));
+        System.out.println(env.getProperty("USERDOMAIN"));
+        System.out.println(env.getProperty("POWERSHELL_DISTRIBUTION_CHANNEL"));
+        System.out.println(env.getProperty("java.version"));
+        System.out.println(env.getProperty("app.env1"));
+        System.out.println(env.getProperty("app.env2"));
+        System.out.println(env.getProperty("app.env3"));
     }
 }
