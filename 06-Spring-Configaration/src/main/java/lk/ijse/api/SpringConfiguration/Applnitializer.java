35a964eda116db9e826fe12793649039e30c325b
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-22 11:21:25 +0530

    updated

---
+++ 06-Spring-Configaration/src/main/java/lk/ijse/api/SpringConfiguration/Applnitializer.java
@@ -0,0 +1,12 @@
+package lk.ijse.api.SpringConfiguration;
+
+/**
+ * @version: v0.0.1
+ * @author: LakshanR
+ * @date: 2/22/2024
+ */
+public class Applnitializer {
+    public static void main(String[] args) {
+
+    }
+}
