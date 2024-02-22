3fa0256ae9bcb2f08c563a4b7bc8c1059f0de7bd
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-22 09:35:38 +0530

    updated

--- 05-Spring_mode/src/main/java/lk/ijse/api/springMode/AppConfig.java
+++ 05-Spring_mode/src/main/java/lk/ijse/api/springMode/AppConfig.java
@@ -1,6 +1,10 @@
 package lk.ijse.api.springMode;
 
-import org.springframework.stereotype.Component;
+import lk.ijse.api.springMode.bean.SpringBeanOne;
+import lk.ijse.api.springMode.bean.SpringBeanTwo;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * @version: v0.0.1
@@ -8,5 +12,12 @@ import org.springframework.stereotype.Component;
  * @date: 2/22/2024
  */
 
+@Configuration
+@ComponentScan
 public class AppConfig {
+
+    @Bean
+    public SpringBeanTwo springBeanTwo(){
+        return new SpringBeanTwo();
+    }
 }
