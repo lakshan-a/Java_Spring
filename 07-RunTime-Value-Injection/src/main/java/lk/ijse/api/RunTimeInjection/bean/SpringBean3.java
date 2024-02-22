0516cc4d3113b3fcf92b040574dd55891d036435
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-22 16:06:54 +0530

    updated

--- 07-RunTime-Value-Injection/src/main/java/lk/ijse/api/RunTimeInjection/bean/SpringBean3.java
+++ 07-RunTime-Value-Injection/src/main/java/lk/ijse/api/RunTimeInjection/bean/SpringBean3.java
@@ -1,9 +1,17 @@
 package lk.ijse.api.RunTimeInjection.bean;
 
+import org.springframework.stereotype.Component;
+
 /**
  * @version: v0.0.1
  * @author: LakshanR
  * @date: 2/22/2024
  */
+
+@Component
 public class SpringBean3 {
+
+    public SpringBean3() {
+        System.out.println("SpringBean3() - Constructor");
+    }
 }
