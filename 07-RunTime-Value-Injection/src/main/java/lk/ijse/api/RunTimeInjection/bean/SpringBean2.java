59e30a51b838ce96839bd73ddc7a39a0cd3834bd
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-22 15:27:48 +0530

    updated

--- 07-RunTime-Value-Injection/src/main/java/lk/ijse/api/RunTimeInjection/bean/SpringBean2.java
+++ 07-RunTime-Value-Injection/src/main/java/lk/ijse/api/RunTimeInjection/bean/SpringBean2.java
@@ -1,9 +1,16 @@
 package lk.ijse.api.RunTimeInjection.bean;
 
+import org.springframework.stereotype.Component;
+
 /**
  * @version: v0.0.1
  * @author: LakshanR
  * @date: 2/22/2024
  */
+
+@Component
 public class SpringBean2 {
+    public SpringBean2() {
+        System.out.println("SpringBean2() - Constructor");
+    }
 }
