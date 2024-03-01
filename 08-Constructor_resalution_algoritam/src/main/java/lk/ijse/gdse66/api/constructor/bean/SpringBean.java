d719a981a1f7587b74e6bf6dadd7a9008807623f
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-03-01 09:06:08 +0530

    updated

--- 08-Constructor_resalution_algoritam/src/main/java/lk/ijse/gdse66/api/constructor/bean/SpringBean.java
+++ 08-Constructor_resalution_algoritam/src/main/java/lk/ijse/gdse66/api/constructor/bean/SpringBean.java
@@ -16,9 +16,9 @@ public class SpringBean {
     private boolean isMale;
     private float temp;
 
-    public SpringBean() {
+    /*public SpringBean() {
         System.out.println("SpringBean() - Constructor");
-    }
+    }*/
 
     public SpringBean(String name, int age, boolean isMale) {
         this.name = name;
